7d6e3d6f97419d672c8fa31616492268f59f63fb
Rongala Gangadhar <rgan9562@gmail.com>
2024-03-10 22:43:16 +0530

    updated index.html

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ let body = document.querySelector(".body");
 head.onclick = () => {
   up.classList.toggle("show");
   down.classList.toggle("show");
-  if (!body.style.display || body.style.display == "none") body.style.display = "block";
+  if (body.style.display == null || body.style.display == "none") body.style.display = "block";
   else body.style.display = "none"
 };
